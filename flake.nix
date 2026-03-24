185e574bb46b56a26ce4cfe08afb59c14a555420
Nick Wisner <wisner.nick@gmail.com>
2026-03-23 22:39:27 -0400

    fix: CI builds

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     sigil-src = {
-      url = "git+file:///home/nick/workspace/sigil";
+      url = "github:sigil-tech/sigil";
       flake = false;
     };
   };
